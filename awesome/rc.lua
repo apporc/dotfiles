fcc7b4fb3de2653f1a699e44a5fd886a0e399c90
apporc <appleorchard2000@gmail.com>
2014-10-08 00:15:44 +0800

    Add fcitx support, adjust xinitrc
    Signed-off-by: apporc <appleorchard2000@gmail.com>

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -473,3 +473,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Autostarts
+awful.util.spawn_with_shell("fcitx")
