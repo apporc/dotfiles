a9cbb1d6e4390cd2584af18f66a553a3b094f817
apporc <appleorchard2000@gmail.com>
2015-12-24 18:43:57 +0800

    new server to hack
    Signed-off-by: apporc <appleorchard2000@gmail.com>

---
+++ scripts/new_remote_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NEW_USER=apporc
+PASSWORD=abc123
+REMOTE_SERVER=$1
+
+ssh root@${REMOTE_SERVER} "adduser ${NEW_USER} && echo ${NEW_USER}:${PASSWORD} | chpasswd"
+ssh-copy-id -i ${HOME}/.ssh/id_rsa.pub ${NEW_USER}@${REMOTE_SERVER}
+scp ../awesome/.tmux.conf ${NEW_USER}@${REMOTE_SERVER}:~
+
+# Host $(echo ${REMOTE_SERVER} | cut -d. -f4)
+cat >> ${HOME}/.ssh/config << EOF
+Host ${REMOTE_SERVER##[0-9]?*\.}
+HostName $REMOTE_SERVER
+    User apporc
+    Port 22
+EOF
