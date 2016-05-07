5ea87dcf166d50de4c20fc596afd21eaabbff44e
apporc <appleorchard2000@gmail.com>
2016-05-07 17:10:40 +0800

    configuration file
    Signed-off-by: apporc <appleorchard2000@gmail.com>

--- scripts/new_remote_server.sh
+++ scripts/new_remote_server.sh
@@ -5,7 +5,7 @@ PASSWORD=abc123
 REMOTE_SERVER=$1
 
 ssh root@${REMOTE_SERVER} "adduser ${NEW_USER} && echo ${NEW_USER}:${PASSWORD} | chpasswd"
-ssh-copy-id -i ${HOME}/.ssh/id_rsa.pub ${NEW_USER}@${REMOTE_SERVER}
+ssh-copy-id -i ${HOME}/.ssh/keys/apporc_ecdsa.pub ${NEW_USER}@${REMOTE_SERVER}
 scp ${HOME}/.tmux.conf ${NEW_USER}@${REMOTE_SERVER}:~
 
 # Host $(echo ${REMOTE_SERVER} | cut -d. -f4)
